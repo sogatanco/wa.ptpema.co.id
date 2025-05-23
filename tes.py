35f081f2a1a2413539a46c350074a790f76f9ffc
pema-dev <mitrapema@gmail.com>
2025-05-23 22:40:45 +0700

    dsgg

--- tes.py
+++ tes.py
@@ -1,14 +1,25 @@
 import requests
+import time
+import threading
 
+# Konfigurasi proxy Tor
 proxies = {
     'http': 'socks5h://127.0.0.1:9050',
     'https': 'socks5h://127.0.0.1:9050'
 }
 
-url = "https://httpbin.org/ip"  # atau https://check.torproject.org
+url = "https://ajnn.net"
 
-try:
-    response = requests.get(url, proxies=proxies, timeout=10)
-    print("IP dari Tor:", response.text)
-except Exception as e:
-    print("Error:", e)
+# Fungsi untuk satu permintaan
+def curl_once(index):
+    try:
+        response = requests.get(url, proxies=proxies, timeout=10)
+        print(f"[{index}] Status: {response.status_code}")
+    except Exception as e:
+        print(f"[{index}] Error:", e)
+
+# Loop 60 kali per detik (setiap 0.0167 detik)
+for i in range(60):
+    t = threading.Thread(target=curl_once, args=(i+1,))
+    t.start()
+    time.sleep(1/60)
