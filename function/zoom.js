e0cca36d6c79ed35fa79892a6f277b8eee985031
pema-dev <mitrapema@gmail.com>
2025-06-23 11:49:34 +0700

    sdgs

--- function/zoom.js
+++ function/zoom.js
@@ -126,7 +126,7 @@ export const createZoomMeetingWithConflict = async (topic, start_time_iso, end_t
 export const deleteZoomMeeting = async (meetingId, accountIdx = 1) => {
     const token = await getZoomToken(accountIdx);
     await axios.delete(
-        `https://api.zoom.us/v2/meetings/${meetingId}`,
+        `https://api.zoom.us/v2/scheduler/schedules/${meetingId}`,
         {
             headers: {
                 Authorization: `Bearer ${token}`,
